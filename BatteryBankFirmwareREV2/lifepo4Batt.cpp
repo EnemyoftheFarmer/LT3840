747befa2a5c5163a85afe6e3cf78f62b38383257
EnemyoftheFarmer <39673402+EnemyoftheFarmer@users.noreply.github.com>
2019-03-08 15:27:14 +0100

    Clean up

--- BatteryBankFirmwareREV2/lifepo4Batt.cpp
+++ BatteryBankFirmwareREV2/lifepo4Batt.cpp
@@ -1,7 +1,18 @@
-#include "Lifepo4Batt.h"
+#include "lifepo4Batt.h"
 #include "Arduino.h"
 
-void Lifepo4Batt::indicateBatLvl()
+int lifepo4Batt::getBattLvl(vBatMeasurePin)
 {
-  
+  //returns the batteries energy level
+  vBat = analogRead(vBatMeasurePin) * .0049;        //converts the ADCs reading of the battery line to a human readable voltage
+  if(vBat >= 3.600){
+    batLevel = 3;
+  }
+  if(vBat < 3.6 && vBat > 2.5){
+    batLevel = 2;
+  }
+  if(vBat < 2.5){
+    batLevel = 1;
+  }
+  return batLevel;
 }
